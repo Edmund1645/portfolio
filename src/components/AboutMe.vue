36fd4a116a1b90f60b85e8dd1848bc71ad1d9784
edmund1645 <edmund.timfon@gmail.com>
2019-08-01 21:08:47 +0100

    complete about me section

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -1,9 +1,15 @@
 <template>
   <section class="space-top">
-    <h1 class="font-thick section-header-text">About Me</h1>
-    <p
-      class="more-about-me section-text"
-    >I build things for the web with pixel-perfect UIs and enjoy solving problems with JavaScript. I also share my knowledge about the web by writing articles. I enjoy listening to music and playing video games too.</p>
+    <div class="about-me-text">
+      <h1 class="font-thick section-header-text">About Me</h1>
+      <p class="more-about-me section-text">
+        I build things for the web with pixel-perfect UIs and enjoy solving problems with JavaScript.
+        I also share my knowledge about the web by writing articles. I enjoy listening to music and playing video games too.
+      </p>
+    </div>
+    <div class="svg-image-section">
+      <img src="../assets/images/coding.svg" alt="illustration of a coder" />
+    </div>
   </section>
 </template>
 
@@ -20,8 +26,35 @@ export default {
 section {
   margin-left: 5%;
   margin-right: 5%;
+  display: flex;
+  justify-content: space-between;
 }
 .more-about-me {
   text-align: left;
+}
+@media screen and (max-width: 425px) {
+  .svg-image-section {
+    display: none;
+  }
+}
+@media screen and (min-width: 768px) {
+  .svg-image-section {
+    margin-top: auto;
+    padding-left: 3rem;
+  }
+  .svg-image-section img {
+    width: 200px;
+  }
+}
+@media screen and (min-width: 1024px) {
+  .about-me-text {
+    padding-right: 7rem;
+  }
+  .svg-image-section img {
+    width: 250px;
+  }
+  .svg-image-section {
+    margin-top: auto;
+  }
 }
 </style>
